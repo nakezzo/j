a7f431fcca95d8ef98c1af4c4902435987560464
nakezzo <ivanshemuda2@gmail.com>
2024-11-02 20:24:31 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = "7730193412:AAFe6CD5GqWb0D6TJBMxdc3DFMsGz_MkrvY" # Токен бота
+CHANNEL_ID = -1002190791524 # ID канала со ставками
+ADMINS = [7646320295] # ID админов
+BET_URL = "t.me/send?start=IVn0ttZb2SIu" # Ссылка на счет
+CRYPTOPAY_TOKEN = "286907:AAAbiflxBaT7iHhBilpHkgYvYAZ3C5GX0do" # Тут CryptoPay токен
+WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
+LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
+LOGS_ID =-1002471007722 # ID канала с оплатами
+
+RULES_LINK = "https://t.me/+jjKp555LFMQwMzk6" # Ссылка на канал с правиламиW
+NEWS_LINK = "https://t.me/+XMqQOXlm5s4yYTFi" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/n1ce_Tag" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "https://t.me/+w9v0r9wB-f82ZmMy" # Ссылка на переходник
+BETS_LINK = "https://t.me/+Wg5jVwo-MB81OGNi" # Ссылка на канал ставок
+CASINO_NAME = "CryptoHub" # Название казино
