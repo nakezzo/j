58070d823e4da1793b1c9314079c755dff81aac9
nakezzo <ivanshemuda2@gmail.com>
2024-11-14 22:31:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ BET_URL = "t.me/send?start=IVUHyST8MJqb" # Ссылка на счет
 CRYPTOPAY_TOKEN = "292406:AADUzPT5ml7dZtF7UBuOnnoi46du2A4UqOm" # Тут CryptoPay токен
 WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
 LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
-LOGS_ID = -1002469070319 # ID канала с оплатами
+LOGS_ID =-1002469070319 # ID канала с оплатами
 
 RULES_LINK = "https://t.me/+TCxjr8zALuMxYTIy" # Ссылка на канал с правиламиW
 NEWS_LINK = "https://t.me/+Tr9fc18HIfdhOWYy" # Ссылка на новостной канал
 OWNER_LINK = "https://t.me/MARS_Sup" # Ссылка на владельца/поддержку
 PEREHOD_LINK = "https://t.me/+FSdg8euMsjNjMWYy" # Ссылка на переходник
-BETS_LINK = "https://t.me/+3_SpFm1XV5RhNWE6" # Ссылка на канал ставок
+BETS_LINK = "https://t.me/+J6IyO6jk7p8yNGMy" # Ссылка на канал ставок
 CASINO_NAME = "CasinoMars" # Название казино
